227894444cff84242b107ed9d6b1dc5ad6e3aaf5
DXcarlos <clopezh@uni.pe>
2021-01-17 00:46:19 -0500

    Agrega parámetro a función de gráfico Se agrega el parámetro para indicar si se desea el gráfico de descripción de clusters en un solo gráfico o de manera separada

--- src/utils/plot_figures.py
+++ src/utils/plot_figures.py
@@ -1,15 +1,19 @@
+import math
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 from sklearn.preprocessing import MinMaxScaler
 
 
-def plot_cluster_polar_figure(df):
+def plot_cluster_polar_figure(df: pd.DataFrame, plot_type: str) -> go.Figure:
     """
     Plot polar graph for cluster description
 
     :param df: original features with a cluster label named cluster
-    :return:
+    :param plot_type: single or join graph
+    :return: figure object
     """
 
     # Separate cluster label from train data to normalize features
@@ -21,10 +25,38 @@ def plot_cluster_polar_figure(df):
 
     # Get mean of each normalized feature
     grouped_df = normalized_df.groupby(['cluster']).mean().reset_index()
-    mean_df = grouped_df.melt(id_vars='cluster', var_name='audio_feature', value_name='mean')
 
-    # Make polar plot
-    fig = px.line_polar(mean_df, r="mean", theta="audio_feature", color="cluster", line_close=True)
-    fig.update_traces(fill='toself')
+    # Create diferent graphs depending on type plot
+    if plot_type == 'join':
+
+        # Create melt dataframe
+        mean_df = grouped_df.melt(id_vars='cluster', var_name='audio_feature', value_name='mean')
+        fig = px.line_polar(mean_df, r="mean", theta="audio_feature", color="cluster", line_close=True,
+                            title='Cluster Composition')
+        fig.update_traces(fill='toself')
+
+    elif plot_type == 'single':
+
+        # Define number of columns and rows
+        n_clusters = df['cluster'].nunique()
+        cols = 3
+        rows = math.ceil(n_clusters / cols)
+
+        # Define figure
+        fig = make_subplots(rows=rows, cols=3, specs=[[{'type': 'polar'}] * 3] * rows)
+
+        for i in range(n_clusters):
+            fig.add_trace(
+                go.Scatterpolar(
+                    name=f"Cluster {i}",
+                    r=grouped_df[grouped_df['cluster'] == i].drop('cluster', axis=1).values[0],
+                    theta=list(train_features.columns),
+                ), i // cols + 1, i % cols + 1)
+
+        fig.update_traces(fill='toself')
+        fig.update_layout(showlegend=True)
+
+    else:
+        raise Exception('Plot type supporting by now : single and join')
 
     return fig
